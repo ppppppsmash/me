5e077518cfeb3e0da8e40fde8116579cd947f267
ppppppsmash <pppppp.work@gmail.com>
2023-06-23 23:59:26 +0900

    mod:　トップ

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import { PROFILE } from '@/constants'
 
 export default function Home() {
   return (
-    <div className='w-full h-[100vh] grid place-content-center relative'>
+    <div className='w-full h-[100vh] relative'>
       <h1 className='text-[2.5rem] translate-y-5 animate-slide-in-sec
-      absolute top-1/2 left-[calc(50%+50px)] min-w-[165px]
-      transform translate-x-[-50%] translate-[calc(-50%-20px)]'>
+      absolute top-1/2 left-[calc(50%+50px)] min-w-[165px] opacity-0
+      transform -translate-x-1/2 translate-[calc(-50%-20px)]
+      '>
         Kurosawa
       </h1>
     </div>
