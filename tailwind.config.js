98fd41388b52122f976496b92fac2d6c16340e0a
ppppppsmash <pppppp.work@gmail.com>
2024-03-26 09:57:09 +0900

    chore: new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'panton': ['panton']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
