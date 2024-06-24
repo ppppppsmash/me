5938a3c449c8e5acb953dac9d465715bc7ce0514
ppppppsmash <pppppp.work@gmail.com>
2024-06-24 10:09:51 +0900

    feat: location api route

---
+++ app/api/location/route.ts
@@ -0,0 +1,44 @@
+// app/api/save-geo/route.js
+import { NextResponse } from 'next/server'
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL,
+  token: process.env.UPSTASH_REDIS_TOKEN,
+})
+
+
+export async function GET() {
+  try {
+    const visitorCount = await redis.get('visitor_count')
+    const lastVisitorCountry = await redis.get('last_visitor_country')
+
+    return new Response(JSON.stringify({
+      visitorCount: visitorCount || 0,
+      lastVisitorCountry: lastVisitorCountry || 'Unknown',
+    }))
+  } catch (error) {
+    console.error('Error retrieving visitor stats:', error)
+    return NextResponse.json({ error: 'Failed to retrieve visitor stats' }, { status: 500 })
+  }
+}
+
+export async function POST(req) {
+  const { country } = await req.json()
+
+  try {
+    // 最新の国情報を保存
+    await redis.set('last_visitor_country', country)
+
+    // 閲覧者数をインクリメント
+    await redis.incr('visitor_count')
+
+    const data = await redis.get('last_visitor_country')
+    console.log(data)
+
+    return new Response(JSON.stringify({ message: 'Geo information saved' }))
+  } catch (error) {
+    console.error('Error saving geo information:', error)
+    return NextResponse.json({ error: 'Failed to save geo information' }, { status: 500 })
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import gsap from 'gsap'
+import useGeoLocation from 'react-ipgeolocation'
 
 const SIDES = ['top', 'right', 'bottom', 'left']
 
@@ -22,6 +23,8 @@ const CONFIG = {
 }
 
 const Home = () => {
+  const [visitorStats, setVisitorStats] = useState({ visitorCount: 0, lastVisitorCountry: 'Unknown' })
+
   useEffect(() => {
     const GENERATE_BEAMS = () => {
       for (const SIDE of SIDES) {
@@ -40,11 +43,11 @@ const Home = () => {
           CONTAINER.appendChild(Object.assign(document.createElement('div'), {
             className: 'beam',
             style: `
-              --hue: ${BEAM.hue};
-              --ar: ${gsap.utils.random(1, 10, 1)};
-              --x: ${BEAM.x};
-              --speed: ${BEAM.speed};
-              --delay: ${BEAM.delay};
+              --hue: ${BEAM.hue}
+              --ar: ${gsap.utils.random(1, 10, 1)}
+              --x: ${BEAM.x}
+              --speed: ${BEAM.speed}
+              --delay: ${BEAM.delay}
             `,
           }))
         }
@@ -70,6 +73,47 @@ const Home = () => {
     UPDATE_MOTION_PREF()
   }, [])
 
+  const location = useGeoLocation()
+
+  useEffect(() => {
+    if (location.country) {
+      const saveGeoInfo = async () => {
+        try {
+          const response = await fetch('/api/location', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ country: location.country }),
+          })
+
+          if (!response.ok) {
+            throw new Error('Failed to save geo information')
+          }
+
+        } catch (error) {
+          console.error('Error saving geo info:', error)
+        }
+      }
+
+      saveGeoInfo()
+    }
+  }, [location.country])
+
+  useEffect(() => {
+    const fetchVisitorStats = async () => {
+      try {
+        const response = await fetch('/api/location')
+        const data = await response.json()
+        setVisitorStats(data)
+      } catch (error) {
+        console.error('Error fetching visitor stats:', error)
+      }
+    }
+
+    fetchVisitorStats()
+  }, [])
+
   return (
     <div className="scene">
       <div className="wrapper">
@@ -78,7 +122,7 @@ const Home = () => {
           animate-[shimmer_2.5s_linear_infinite]">
             <h1 className="text-[2.5rem] translate-y-5 animate-slide-in-title font-bold
               min-w-[165px] opacity-0 pr-2 transform py-8 rounded-[15px] bg-white dark:bg-black font-panton text-black dark:text-white"
-            >Kurosawa&#39;s portfolio</h1>
+            >Kurosawa&#39s portfolio</h1>
         </article>
         
         <div className="warp">
@@ -87,6 +131,8 @@ const Home = () => {
           ))}
         </div>
       </div>
+      {/* <p>Total Visitors: {visitorStats.visitorCount}</p>
+      <p>Last Visitor Country: {visitorStats.lastVisitorCountry}</p> */}
     </div>
   )
 }
