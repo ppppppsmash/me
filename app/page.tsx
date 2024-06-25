3c4ca8d3bd4842b047179d6b4248365923047115
ppppppsmash <pppppp.work@gmail.com>
2024-06-25 21:01:28 +0900

    feat: typewriter component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-const Home = () => {
+export default function Home() {
   return (
     <div>
       <div className="flex flex-col justify-center items-center">
         <h1 className="absolute top-1/2 -translate-y-1/2 z-10 text-5xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
-          Kurosawa's Portfolio
+          Kurosawa{("'")}s Portfolio
         </h1>
       </div>
     </div>
   )
 }
-
-export default Home
