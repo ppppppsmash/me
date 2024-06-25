6a39fb3c1a58274d27c36a828ffbb41e3e87f1a4
ppppppsmash <pppppp.work@gmail.com>
2024-06-25 19:06:28 +0900

    fix: text color

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ const Home = () => {
           bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.teal.400)_30%,theme(colors.slate.100)_40%,theme(colors.amber.600)_20%)]
           animate-[shimmer_2.5s_linear_infinite]">
             <h1 className="text-[2.5rem] translate-y-5 animate-slide-in-title font-bold
-              min-w-[165px] opacity-0 pr-2 transform py-8 rounded-[15px] bg-black font-panton text-black dark:text-white"
+              min-w-[165px] opacity-0 pr-2 transform py-8 rounded-[15px] bg-black font-panton text-white"
             >Kurosawa&#39;s portfolio</h1>
         </article>
         
