30312e3cab5c09e77b9b5278b14f2ac71ea12950
kurosawa <pppppp.work@gmail.com>
2025-03-23 11:31:45 +0900

    feat: Add link preview components

--- next.config.js
+++ next.config.js
@@ -1,23 +1,34 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['placehold.jp', 'res.cloudinary.com', 'avatars.githubusercontent.com'],
+    domains: [
+      "placehold.jp",
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "aceherblab.com",
+      "lucent-fairy-5f9ca0.netlify.app",
+      "game-survey-demo.vercel.app",
+      "page-speed-measurement-nextapp-oclbewqdfa-an.a.run.app",
+      "next-sanity-six-sooty.vercel.app",
+      "tvgame-survey.netlify.app",
+      "my-task-management-gamma.vercel.app",
+    ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/29497177'
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/29497177"
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.simpleicons.org',
-        port: '',
-        pathname: '/**'
-      }
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+        port: "",
+        pathname: "/**"
+      },
     ]
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
